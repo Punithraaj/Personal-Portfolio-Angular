b9ec897f9e961318fa568adbce995854c07fa8c5
Punithraj <punithraj14@gmail.com>
2020-05-23 16:59:16 +0530

    Deployment Commit

--- src/app/profile/contact/contact.component.ts
+++ src/app/profile/contact/contact.component.ts
@@ -8,37 +8,12 @@ import { environment } from '../../../environments/environment';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
-  public email='punithraaj14@gmail.com';
-  snotifyConfig = environment.snotifyConfig;
-  model: any = {};
+  export class ContactComponent implements OnInit {
+ 
+    model: any = {};
   
-  constructor(
-    private profile: ProfileService,
-    private snotify: SnotifyService
-    
-  ) { }
-
-  ngOnInit() {
-  }
-
-  contact() {
-    this.profile.contactus({
-      from: this.model.email,
-      to: this.email,
-      name: this.model.name,
-      subject: this.model.subject,
-      text:this.model.subject
-    }).subscribe(data => {
-      if (data.status) {
-        this.snotify.success(data.message, 'Success', this.snotifyConfig);
-      } else {
-        this.snotify.warning(data.message, 'Warning', this.snotifyConfig);
-      }
-    }, err => {
-      this.snotify.error('Something went wrong. Try again later.', 'Error', this.snotifyConfig);
-    });
-  }
-
-
+    constructor(){}
+  
+    ngOnInit() {
+       }
 }
