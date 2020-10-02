74bdf6b3eb6971a3dd47add59a4b4b832448155c
Punithraj <punithraj14@gmail.com>
2020-10-02 14:41:04 +0530

    Added Email Option for contact form

--- src/app/profile/contact/contact.component.ts
+++ src/app/profile/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders} from '@angular/common/http';
 import { ProfileService } from '../profile.service';
 import { SnotifyService } from 'ng-snotify';
 import { environment } from '../../../environments/environment';
@@ -12,8 +14,20 @@ import { environment } from '../../../environments/environment';
  
     model: any = {};
   
-    constructor(){}
+    constructor(
+      private http: HttpClient
+    ){}
   
     ngOnInit() {
        }
+       onSubmit(name, subject, email, message) {
+          const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+          this.http.post('https://formspree.io/f/mwkwpzve',
+            { name: name, subject: subject, replyto: email, message: message },
+            { 'headers': headers }).subscribe(
+              response => {
+                console.log(response);
+              }
+            );
+        }  
 }
